eb836e4276279dde27a43a805d80e7a10c76e285
Natanael Lino <natannlino@gmail.com>
2023-04-14 16:45:06 -0300

    resolução d011

--- desafios/d011/index.php
+++ desafios/d011/index.php
@@ -1,1 +1,36 @@
-pega um preço e imprime um reajuste de preço de acordo com a porcentagem escolhida pelo usuario
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Desafio Raiz</title>
+</head>
+<body>
+    <?php
+        //pega um preço e imprime um reajuste de preço de acordo com a porcentagem escolhida pelo usuario 
+        $preco = $_GET['preco'] ?? 0;
+        $percentual = $_GET['percentual'] ?? 0;
+    ?>
+    <main>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="preco">Preço do Produto (R$)</label>
+            <input type="number" name="preco" id="preco" value="<?=$preco?>">
+            <label for="percentual">Qual o percentual de reajuste?</label>
+            <input type="number" name="percentual" id="percentual" value="<?=$percentual?>"> 
+            <input type="submit" value="Qual será a minha idade?">
+        </form>
+    </main>
+
+    <section id="resultado">
+        <h2>Resultado do Reajuste</h2>
+        <?php 
+            $reajuste = (($preco * $percentual)/100);
+            $reajuste += $preco;
+
+            echo "O produto que custava $preco, com <strong>R$" . $percentual . "% de aumento</strong> vai passar a custar <strong>R$" . number_format($reajuste, 2, ",", ".") . "</strong> a partir de agora.";
+        ?>
+    </section>
+</body>
+</html>
