ea87d57d053e042ebfd1283a0a2d3b695fa74711
Natanael Lino <natannlino@gmail.com>
2023-05-02 19:19:36 -0300

    resolução do d010

--- desafios/d010/index.php
+++ desafios/d010/index.php
@@ -10,26 +10,27 @@
 <body>
     <?php
         //calcula a sua idade com o ano de nascimento e o ano atual 
+        $atual = date("Y");
         $nascimento = $_GET['nascimento'] ?? 0;
-        $ano = $_GET['ano'] ?? 0;
+        $ano = $_GET['ano'] ?? $atual;
     ?>
     <main>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="nascimento">Em que ano você nasceu?</label>
-            <input type="number" name="nascimento" id="nascimento" value="<?=$nascimento?>">
-            <label for="ano">Quer saber sua idade em que ano? (Atualmente estamos em <strong><?=date("Y")?></strong>)</label>
-            <input type="number" name="ano" id="ano" value="<?=$ano?>"> 
+            <input type="number" name="nascimento" id="nascimento" min="1900" max="<?=$atual?>" value="<?=$nascimento?>">
+            <label for="ano">Quer saber sua idade em que ano? (Atualmente estamos em <strong><?=$atual?></strong>)</label>
+            <input type="number" name="ano" id="ano" min="1900" value="<?=$ano?>"> 
             <input type="submit" value="Qual será a minha idade?">
         </form>
     </main>
 
     <section id="resultado">
-        <h2>Resultado</h2>
         <?php 
             $idade = $ano - $nascimento;
-
-            echo "Quem nasceu em $nascimento vai ter <strong>" . $idade . " anos </strong> em $ano";
+            //echo "Quem nasceu em $nascimento vai ter <strong>" . $idade . " anos </strong> em $ano";
         ?>
+        <h2>Resultado</h2>
+        <p>Quem nasceu em <?=$nascimento?> vai ter <strong><?=$idade?> anos</strong> em <?=$ano?></p>
     </section>
 </body>
 </html>
