4f77bee05993be5f7a954785efe4e86da701e866
Natanael Lino <natannlino@gmail.com>
2023-07-28 16:02:50 -0300

    atualização e correção do ex008

--- exerciciosLogica/ex008/index.php
+++ exerciciosLogica/ex008/index.php
@@ -1,13 +1,13 @@
 <?php 
 
     function calcularImposto($salario){
-        if($salario <= 1903.08){
+        if($salario <= 1903.98){
             $imposto = 'ISENTO';
-        } elseif($salario > 1903.08 && $salario <= 2826.65){
+        } elseif($salario >= 1903.99 && $salario <= 2826.65){
             $imposto = ($salario * 7.5) / 100;
-        } elseif($salario > 2826.65 && $salario <= 3751.06){
+        } elseif($salario >= 2826.66 && $salario <= 3751.05){
             $imposto = ($salario * 15) / 100;
-        } elseif($salario > 3751.06 && $salario <= 4664.68){
+        } elseif($salario >= 3751.06 && $salario <= 4664.08){
             $imposto = ($salario * 22.5) / 100;
         } else{
             $imposto = ($salario * 27.5) / 100;
