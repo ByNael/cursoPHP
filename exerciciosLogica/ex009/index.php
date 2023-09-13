08800c7b9bbb1f580b6cf9d0d904ecadcf58e67a
Natanael Lino <natannlino@gmail.com>
2023-09-13 13:28:12 -0300

    aprimorando a resolução do ex009

--- exerciciosLogica/ex009/index.php
+++ exerciciosLogica/ex009/index.php
@@ -1,7 +1,18 @@
 <?php 
     // tabuada simples com entrada do usuario
 
-    $numeroTabuada = readline('Digite o número que você quer saber a tabuada: ');
+    do{
+        $numeroTabuada = readline('Digite o número que você quer saber a tabuada (1 a 10): ');
+        if($numeroTabuada < 1 || $numeroTabuada > 10){
+            echo 'O número deve estar entre 1 e 10' . PHP_EOL;
+        }
+    }while($numeroTabuada < 1 || $numeroTabuada > 10);
+    
+    
+    
+        //if($numeroTabuada < 1 || $numeroTabuada > 10){
+      //  die('O numero tem que estar entre 1 e 10');
+    //}
 
     for($i = 1; $i <= 10; $i++){
         echo "$i x $numeroTabuada = " . ($i*$numeroTabuada) . PHP_EOL;
